d1889ade47c62c192c1ad317aa71d186366d70ca
马场郭德纲\孙毅 <sy739507690@gmail.com>
2025-09-05 14:22:50 +0800

    feat: 新增调度器实现

--- react/react-demo/schedule.mjs
+++ react/react-demo/schedule.mjs
@@ -1,36 +1,78 @@
-// 3. commit阶段
-function commitRoot(wip_fiber) {
-  wip_fiber.deletions && wip_fiber.deletions.forEach(commitWork)
-  commitWork(wip_fiber.child)
-  current_fiber = wip_fiber
-  wip_fiber = null
-}
+// 任务优先级
+const ImmediatePriority = 1
+const UserBlockingPriority = 2
+const NormalPriority = 3
+const LowPriority = 4
+const IdlePriority = 5
 
-function commitWork(fiber) {
-  if (!fiber) {
-    return
+class Scheduler {
+  constructor() {
+    this.taskQueue = [] // 任务队列
+    this.isPerformingWork = false // 是否正在执行任务
+    const channel = new MessageChannel()
+    this.port = channel.port2
+    channel.port1.onmessage = this.performWork.bind(this)
   }
 
-  const domParent = fiber.parent.dom
-  if (fiber.effectTag === 'PLACEMENT' && fiber.dom) {
-    domParent.appendChild(fiber.dom)
-  } else if (fiber.effectTag === 'UPDATE' && fiber.dom) {
-    // 更新属性
-    const oldProps = fiber.alternate.props
-    const newProps = fiber.props
-    for (const key in oldProps) {
-      if (key !== 'children' && !(key in newProps)) {
-        domParent[key] = ''
-      }
+  /**
+   *
+   * @param {优先级} priority
+   * @param {*} callback
+   */
+  scheduleCallback(priority, callback) {
+    const curTime = performance.now()
+    let timeOut
+    switch (priority) {
+      case ImmediatePriority:
+        timeOut = -1
+        break
+      case UserBlockingPriority:
+        timeOut = 250
+        break
+      case NormalPriority:
+        timeOut = 5000
+        break
+      case LowPriority:
+        timeOut = 10000
+        break
+      case IdlePriority:
+        timeOut = 1000000
+        break
     }
-    for (const key in newProps) {
-      if (key !== 'children' && oldProps[key] !== newProps[key]) {
-        domParent[key] = newProps[key]
-      }
+    const task = {
+      callback,
+      priority,
+      startTime: curTime,
+      expirationTime: curTime + timeOut,
     }
-  } else if (fiber.effectTag === 'DELETION') {
-    domParent.removeChild(fiber.dom)
+    this.taskQueue.push(task)
+    this.taskQueue.sort((a, b) => a.expirationTime - b.expirationTime)
+
+    this.port.postMessage({ timeOut })
+  }
+
+  performWork() {
+    if (this.isPerformingWork) return
+    this.isPerformingWork = true
+    let currentTask = this.taskQueue.shift()
+    while (currentTask) {
+      const callback = currentTask.callback
+      currentTask.callback = null
+      const continuationCallback = callback()
+      if (continuationCallback) {
+        currentTask.callback = continuationCallback
+        this.taskQueue.unshift(currentTask)
+      }
+      currentTask = this.taskQueue.shift()
+    }
+    this.isPerformingWork = false
   }
 }
 
-export { commitRoot, commitWork }
+const scheduler = new Scheduler()
+scheduler.scheduleCallback(NormalPriority, () => {
+  console.log('普通优先级任务')
+})
+scheduler.scheduleCallback(ImmediatePriority, () => {
+  console.log('立即执行的任务')
+})
