0a62a0597f730ff2d4995d29d2363185462e95c8
马场郭德纲\孙毅 <sy739507690@gmail.com>
2025-09-11 18:34:26 +0800

    feat: 补充二叉树算法题

--- leetcode/tree/二叉搜索树的修改与改造/修剪二叉搜索树.js
+++ leetcode/tree/二叉搜索树的修改与改造/修剪二叉搜索树.js
@@ -1,1 +1,14 @@
 // 给定一个二叉搜索树，同时给定最小边界L 和最大边界 R。通过修剪二叉搜索树，使得所有节点的值在[L, R]中 (R>=L) 。
+
+const trimBST = function (root, L, R) {
+  if (!root) return null
+  if (root.val < L) {
+    return trimBST(root.right, L, R)
+  } else if (root.val > R) {
+    return trimBST(root.left, L, R)
+  } else {
+    root.left = trimBST(root.left, L, R)
+    root.right = trimBST(root.right, L, R)
+    return root
+  }
+}
--- leetcode/tree/求二叉搜索数的属性/二叉搜索树的最小绝对差.js
+++ leetcode/tree/求二叉搜索数的属性/二叉搜索树的最小绝对差.js
@@ -1,1 +1,17 @@
 // 给你一棵所有节点为非负值的二叉搜索树，请你计算树中任意两节点的差的绝对值的最小值。
+
+const getMinimumDifference = function (root) {
+  let min = Infinity,
+    prev = -1
+  const dfs = node => {
+    if (!node) return
+    dfs(node.left)
+    if (prev !== -1) {
+      min = Math.min(min, Math.abs(node.val - prev))
+    }
+    prev = node.val
+    dfs(node.right)
+  }
+  dfs(root)
+  return min
+}
