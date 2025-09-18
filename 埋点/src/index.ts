2ae2de2306066d2248a2903154ac98aca49ad123
马场郭德纲\孙毅 <sy739507690@gmail.com>
2025-09-18 15:13:26 +0800

    refactor: 重构埋点代码

--- 埋点/src/index.ts
+++
@@ -1,8 +0,0 @@
-export { Analytics } from './analytics';
-export { Utils } from './utils';
-export { DataCollector } from './collector';
-export * from './types';
-
-// 默认导出
-import { Analytics } from './analytics';
-export default Analytics;
